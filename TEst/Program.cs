f05c0d978d94f6f33c00021537db338a09f287aa
mrchatib <a2308929@institutmontilivi.cat>
2024-01-31 20:52:54 +0100

    Ayoub 123

--- TEst/Program.cs
+++ TEst/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Hello, Montilivi!");
             Console.WriteLine("I'm learning GitHub");
+            Console.WriteLine("Hola guapo, soy el Younes");
+            Console.WriteLine("1+1");
         }
     }
 }
